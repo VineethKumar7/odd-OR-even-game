780a200c5e73f90f44d5a0e0c0f696ff0a8f8bae
“Vineeth” <k.vineeth26@gmail.com>
2018-10-01 00:23:03 +0530

    Starting for the game code

--- oddOReven.cpp
+++ oddOReven.cpp
@@ -4,41 +4,75 @@
 
 using namespace std;
 
+int isOddNumber(int userChoise, int systemChoise){
+  int oddNum=1;
+  return (userChoise + systemChoise)%2 != 0;
+}
+int isEvenNumber(int userChoise, int systemChoise){
+  int evenNum=2;
+  return (userChoise + systemChoise)%2 == 0;
+}
+
 int main()
 {
-    char name[30];
-    int age;
+    char player1[30], oddEven[4];
     fstream file;
-
-    file.open("aaa.txt",ios::out);
+    int age;
+    int userChoise,systemChoise,won;
+    file.open("Game Score Card.txt",ios::out);
     if(!file)
     {
       cout<<"Error in creating file.."<<endl;
       return 0;
     }
-    cout<<"\nFile created successfully."<<endl;
+    //Terminal instructions
+    cout<<"\n \t------ODD OR EVEN GAME------\t"<<endl;
 
-    //read values from kb
-    cout<<"Enter your name: ";
-    cin.getline(name,30);
-    cout<<"Enter age: ";
-    cin>>age;
+    //read values from User
+    cout<<"Enter the player name ";
+    cin.getline(player1,30);
+    cout<<"\n Choose option \n 1. ODD \n 2.EVEN \n"; //Choise
+    cin>>oddEven;
+    if('ODD' == oddEven || 'odd' == oddEven || '1' == oddEven){
+      userChoise=1;
+      systemChoise = rand() % 2 + 1; //random number between 1 and 2
+      /*
+        odd(userChoise + systemChoise) or eveb similar to the player oddEven then he won.
+      */
+      if((userChoise+systemChoise)%2 == 0){//
+
+      }else{
+
+      }
+    }else if('EVEN' == oddEven || 'even' == oddEven || '2' == oddEven){
+      userChoise=1;
+      systemChoise = rand() % 2 + 1; //random number between 1 and 2
+      if(isOddNumber(userChoise,systemChoise)){
+
+      }else{
+
+      }
+    }
+
     //write into file
-    file<<name<<" "<<age<<endl;
+    file<<player1<<" "<<age<<endl;
+    file<<player1<<" "<<age<<endl;
 
     file.close();
     cout<<"\nFile saved and closed succesfully."<<endl;
 
     //re open file in input mode and read data
     //open file
-    file.open("aaa.txt",ios::in);
+    file.open("Game Score Card.txt",ios::in);
     if(!file){
         cout<<"Error in opening file..";
         return 0;
     }
-    file>>name;
+    file>>player1;
     file>>age;
-
-    cout<<"Name: "<<name<<",Age:"<<age<<endl;
+    cout<<"Name: "<<player1<<",Age:"<<age<<endl;
+    file>>player1;
+    file>>age;
+    cout<<"Name: "<<player1<<",Age:"<<age<<endl;
     return 0;
 }

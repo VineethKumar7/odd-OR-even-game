80c9538621fc72b82afff5345c35386e0abc973e
“Vineeth” <k.vineeth26@gmail.com>
2018-10-01 22:52:01 +0530

    1/4 skelton

--- oddOReven.cpp
+++ oddOReven.cpp
@@ -3,21 +3,19 @@
 #include <fstream>
 
 using namespace std;
+int userOddEven(int num){
+  return num%2!=0;
+}
+void current_game(int game_Array[]){
 
-int oddOREven(int Number, int systemChoise){
-  int Num=2; // By default it is even number
-  if((userChoise + systemChoise)%2 != 0){
-    Num=1;
-  }
-  return Num;
 }
 
 int main()
 {
     char player1[30], oddEven[4];
     fstream file;
-    int Number;
-    int userChoise,systemChoise,won;
+    int Number,flag=1,batting,game_Array[1];
+    int Check,systemChoise,won;
     file.open("Game Score Card.txt",ios::out);
     if(!file)
     {
@@ -30,39 +28,49 @@ int main()
     //read values from User
     cout<<"Enter the player name ";
     cin.getline(player1,30);
-    cout<<"\n Enter a number between 1-6 \n"; //Choise
-    cin>>Number;
-    //For ODD number
-    if(Number % 2 != 0 ){
-      userChoise=1; // Note here 1 indicates it is a odd Number
-      systemChoise = rand() % 2 + 1; //random number between 1 and 2
+    while(flag !=0){
+      cout<<"\n Enter a number between 1-6 \n"; //Choise
+      cin>>Number;
 
-      if(oddOREven(Number) == userChoise){//
-        cout<<"\n Player Won the choise \n";
-      }else{
-        cout<<"\n System Won the choise \n";
+      check1= userOddEven(Number);
+      systemChoise = rand() % 6 + 1; //random number between 1 and 6
+      check2=userOddEven(systemChoise);
+
+      // This will check whether both the inputs are equal or not.
+      if(check1 != check2){
+        flag =1;
       }
     }
-
-    //write into file
-    file<<player1<<" "<<age<<endl;
-    file<<player1<<" "<<age<<endl;
-
-    file.close();
-    cout<<"\nFile saved and closed succesfully."<<endl;
-
-    //re open file in input mode and read data
-    //open file
-    file.open("Game Score Card.txt",ios::in);
-    if(!file){
-        cout<<"Error in opening file..";
-        return 0;
+    check3 = (Number + systemChoise) % 2;
+  if(check1 == check3){
+    cout<<"\n Player Won the toss \n";
+    cout<<"\n Choose 1 for batting and 0 for bowling \n";
+    cin>>batting;
+    if(batting==1){
+      cout<<"Player Won the toss and coose to batting";
+      file<<"player1 Won the toss and"<<"choose batting"<<endl;
+      game_Array={1,0};
+      current_game(game_Array);
+    }else{
+      cout<<"Player Won the toss and choose to bowiling";
+      file<<"player1 Won the toss and"<<"choose bowling"<<endl;
+      game_Array={0,1};
+      current_game(game_Array);
     }
-    file>>player1;
-    file>>age;
-    cout<<"Name: "<<player1<<",Age:"<<age<<endl;
-    file>>player1;
-    file>>age;
-    cout<<"Name: "<<player1<<",Age:"<<age<<endl;
-    return 0;
+
+  }else{
+    cout<<"\n System won the choise \n";
+    batting= rand()%1;
+    if(batting==1){
+      cout<<"System Won the toss and choose batting";
+      file<<"System Won the toss and "<<"choose batting"<<endl;
+      game_Array={0,1};
+      current_game(game_Array);
+  }else{
+    file<<"System Won the toss and choose bowling"<<endl;
+    file<<"System Won the toss and"<<"choose bowling"<<endl;
+    game_Array={1,0};
+    current_game(game_Array);
+  }
+
 }

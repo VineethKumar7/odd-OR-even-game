b8aa1b2f45ae1f47a7f98aca68f843041bf58af2
VineethKumar7 <k.vineeth26@gmail.com>
2018-10-22 06:00:54 -0700

    Update oddOReven.cpp

--- oddOReven.cpp
+++ oddOReven.cpp
@@ -1,10 +1,10 @@
 //C++ program to write and read values using variables in/from file.
-#include <iostream>
-#include <fstream>
+#include <iostream.h>
+#include <conio.h>
+#include <fstream.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-using namespace std;
 int userOddEven(int num){
   return num%2!=0;
 }
@@ -32,7 +32,7 @@ int main(){
   int result[1];
   int Number,flag1=0,flag2=0,flag3=0,batting,game_Array[1];
   int check1,check2,check3,systemChoise,won;
-  file.open("Game Score Card.txt",ios::out);
+  file.open("GAMESCOR.txt",ios::out|ios::app);
   if(!file)
   {
     cout<<"Error in creating file.."<<endl;
@@ -82,12 +82,16 @@ game_Array[1] is for System
     if(batting==1){
       cout<<"Player Won the toss and coose to batting";
       file<<"player1 Won the toss and"<<"choose batting"<<endl;
+      file<<"player1: is "<<player1<<endl;
+      file<<"player2: is System"<<endl;
       game_Array[0]=1;
       game_Array[1]=0;
       current_game(game_Array,result);
     }else{
       cout<<"Player Won the toss and choose to bowiling";
       file<<"player1 Won the toss and"<<"choose bowling"<<endl;
+      file<<"player1: is System"<<endl;
+      file<<"player12: is "<<player1<<endl;
       game_Array[0]=0;
       game_Array[1]=1;
       current_game(game_Array,result);
@@ -98,12 +102,16 @@ game_Array[1] is for System
     if(batting==1){
       cout<<"System Won the toss and choose batting";
       file<<"System Won the toss and "<<"choose batting"<<endl;
+      file<<"player1: is System"<<endl;
+      file<<"player12: is "<<player1<<endl;
       game_Array[0]=0;
       game_Array[1]=1;
       current_game(game_Array,result);
   }else{
-    file<<"System Won the toss and choose bowling"<<endl;
-    file<<"System Won the toss and"<<"choose bowling"<<endl;
+    cout<<"System Won the toss and choose bowling";
+    file<<"System Won the toss and "<<"choose bowling"<<endl;
+    file<<"player1: is "<<player1<<endl;
+    file<<"player2: is System"<<endl;
     game_Array[0]=1;
     game_Array[1]=0;
     current_game(game_Array,result);
@@ -111,26 +119,25 @@ game_Array[1] is for System
   }
   //closing the file
   file.close();
+  getch();
   return 0;
 }
 
 void current_game(int game_Array[1],int result[1]){
-  fstream file; //object of fstream class
+  fstream file2; //object of fstream class
   int value,flag=0,systemChoise,totalScore1=0,totalScore2=0,out=0;
   //opening file "sample.txt" in out(write) mode
-  file.open("Game Score Card.txt",ios::app);
+  file2.open("GAMESCOR.txt",ios::out|ios::app);
 
-  if(!file)
+  if(!file2)
   {
-      cout<<"Error in creating file!!!"<<endl;
+      cout<<"Error in creating file!!!";
   }else{
-    cout<<"\n Game starts here..... \n"<<endl;
-    //write text into file
-    /* out = 0 means the player is not out by default the player is not out.
-      out = 1 means that the player is out
-    */
+    cout<<"\n Game starts here..... \n";
+
     //-----------------First Game -----------------------
-    file<<"------SCORE CARD------"<<endl;
+    out=0;
+    file2<<"------SCORE CARD 1------"<<endl;
     while(!out){
       flag=0;
       while (flag==0) {
@@ -146,23 +153,23 @@ void current_game(int game_Array[1],int result[1]){
       if(value == systemChoise)
       {
         out =1;
-        file<<"OUT"<<endl;
+        file2<<"OUT"<<endl;
       }else{
-        file<<value<<endl;
+        file2<<value<<endl;
       }
     }
-    file<<"\n The Total Score for palyer 1 : "<<totalScore1<<endl;
+    file2<<" The Total Score for palyer 1 : "<<totalScore1<<endl;
     cout<<"\n The total score of player 1 is "<<totalScore1;
     if(game_Array[1] == 1){
       cout<<"\n The second player is Computer";
-      file<<"\n The second player is Computer"<<endl;
+      file2<<" The second player is Computer"<<endl;
     }else{
       cout<<"\n The second player is Player";
-      file<<"\n The second player is Player"<<endl;
+      file2<<" The second player is Player"<<endl;
     }
     // -------------Second GAME-----------------
     out=0;
-    file<<"------SCORE CARD------"<<endl;
+    file2<<"------SCORE CARD 2------"<<endl;
     while(!out){
       flag=0;
       while (flag==0) {
@@ -181,12 +188,12 @@ void current_game(int game_Array[1],int result[1]){
       if(value == systemChoise)
       {
         out =1;
-        file<<"OUT"<<endl;
+        file2<<"OUT"<<endl;
       }else{
-        file<<value<<endl;
+        file2<<value<<endl;
       }
     }
-    file<<"\n The Total Score for palyer 2 : "<<totalScore2<<endl;
+    file2<<"The Total Score for palyer 2 : "<<totalScore2<<endl;
     cout<<"\n The total score of player 2 is "<<totalScore2;
     if(game_Array[0]==1){//If user is batting and have high score then
       if(totalScore1>totalScore2 ){
@@ -203,13 +210,13 @@ void current_game(int game_Array[1],int result[1]){
   }
     if(result[0]==1){
       cout<<"\n User Won the match";
-      file<<"User Won the match"<<endl;
+      file2<<"User Won the match"<<endl;
     }else{
       cout<<"\n System Won the match";
-      file<<"System Won the match"<<endl;
+      file2<<"System Won the match"<<endl;
     }
     //closing the file
-    file.close();
+    file2.close();
   }
 
 }
